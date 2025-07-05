d0ce302fe48b8f949df9394b8d937fe35c7bf42c
YaolongLiu <Yaolong3752@gmail.com>
2025-07-05 12:59:51 -0700

    update

--- Week9/SimpleCalculator.py
+++ Week9/SimpleCalculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 first_name=float(input("What is the first number: "))
-activity = input("What activity? ( + - * / )")
+activity = input("What activity? ( + - * / ): ")
 second_num = float(input("What is the second number: "))
 
 if activity == "+":
